f00176d2e40aac2b765d9b36f20b4d038b335868
Romelolopez <48486610+Romelolopez@users.noreply.github.com>
2022-03-01 08:46:00 -0500

    Update PalindromeNumber.js

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -2,6 +2,11 @@
 //An integer is a palindrome when it reads the same backward as forward.
 //For example, 121 is a palindrome while 123 is not.
 
+// Example 1:
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+
 var isPalindrome = function(x) {
 
     //Convert number to array
